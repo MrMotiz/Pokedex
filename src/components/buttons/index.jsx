234308930931a6579f0f371611a54c64d6498154
MrMotiz <108729772+MrMotiz@users.noreply.github.com>
2023-01-05 12:46:55 +0000

    fix useEffect missing dependecies

--- src/components/buttons/index.jsx
+++ src/components/buttons/index.jsx
@@ -1,6 +1,7 @@
 import { Button, BackButton } from "./styles";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
+import {typeColors,typeColorsBorders } from "../../utils/noPokemon"
 
 export const FilterButton = ({name, handleSearch})=>{
 
@@ -9,6 +10,8 @@ export const FilterButton = ({name, handleSearch})=>{
     const borderColors = Object.values(typeColorsBorders);
     const [typeColor, setTypeColor] = useState("");
     const [borderColor, setBorderColor] = useState("");
+    const tempColors = useRef();
+
     const setColors = () =>{
 
         for (let i = 0; i < types.length; i++) {
@@ -18,9 +21,10 @@ export const FilterButton = ({name, handleSearch})=>{
             }
         }
     }
+    tempColors.current = setColors
 
     useEffect(() => {
-        setColors();
+        tempColors.current();
     }, []);
 
   
@@ -36,47 +40,3 @@ export const ReturnButton = ({handleClick}) =>{
     return <BackButton onClick={handleClick}/>
 }
 
-const typeColors = {
-    electric: '#F8D030',
-    normal: '#a8a878',
-    fire: '#f08030',
-    water: '#6890F0',
-    ice: '#98D8D8',
-    rock: '#B8A038',
-    flying: '#A890F0',
-    grass: '#78C850',
-    psychic: '#F85888',
-    ghost: '#705898',
-    bug: '#A8B820',
-    poison: '#A040A0',
-    ground: '#E0C068',
-    dragon: '#7038F8',
-    dark: "#705848",
-    steel: '#B8B8D0',
-    fairy: "#EE99AC",
-    fighting: '#C03028',
-    unknown: "#68A090",
-    default: '',
-};
-const typeColorsBorders = {
-    electric: '#A1871F',
-    normal: '#6D6D4E',
-    fire: '#9C531F',
-    water: '#445E9C',
-    ice: '#638D8D',
-    rock: '#786824',
-    flying: '#6D5E9C',
-    grass: '#4E8234',
-    psychic: '#A13959',
-    ghost: '#493963',
-    bug: '#6D7815',
-    poison: '#682A68',
-    ground: '#927D44',
-    dragon: '#4924A1',
-    dark: "#49392F",
-    steel: '#787887',
-    fairy: "#9B6470",
-    fighting: '#7D1F1A',
-    unknown: "#9DC1B7",
-    default: '#2A1A1F',
-};
--- src/utils/noPokemon.js
+++ src/utils/noPokemon.js
@@ -15,6 +15,51 @@ const noPreviewPokemon = {
 	}
 ;
 
+const typeColors = {
+    electric: '#F8D030',
+    normal: '#a8a878',
+    fire: '#f08030',
+    water: '#6890F0',
+    ice: '#98D8D8',
+    rock: '#B8A038',
+    flying: '#A890F0',
+    grass: '#78C850',
+    psychic: '#F85888',
+    ghost: '#705898',
+    bug: '#A8B820',
+    poison: '#A040A0',
+    ground: '#E0C068',
+    dragon: '#7038F8',
+    dark: "#705848",
+    steel: '#B8B8D0',
+    fairy: "#EE99AC",
+    fighting: '#C03028',
+    unknown: "#68A090",
+    default: '',
+};
+const typeColorsBorders = {
+    electric: '#A1871F',
+    normal: '#6D6D4E',
+    fire: '#9C531F',
+    water: '#445E9C',
+    ice: '#638D8D',
+    rock: '#786824',
+    flying: '#6D5E9C',
+    grass: '#4E8234',
+    psychic: '#A13959',
+    ghost: '#493963',
+    bug: '#6D7815',
+    poison: '#682A68',
+    ground: '#927D44',
+    dragon: '#4924A1',
+    dark: "#49392F",
+    steel: '#787887',
+    fairy: "#9B6470",
+    fighting: '#7D1F1A',
+    unknown: "#9DC1B7",
+    default: '#2A1A1F',
+};
 
 
-export{noPreviewPokemon, noNextPokemon};
+
+export{noPreviewPokemon, noNextPokemon,typeColors, typeColorsBorders};

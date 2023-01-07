cd80f3783dd056421bd275f2c3ab1c818fcd8094
MrMotiz <108729772+MrMotiz@users.noreply.github.com>
2023-01-07 15:56:51 +0000

    remove useRef

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
 import { FullDetails, PreviewNextPoke } from "../components"
